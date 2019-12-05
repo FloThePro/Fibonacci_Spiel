379fd30a08c302e45b3d10253a68c3853f69261e
FloThePro <florian.uhl1998@web.de>
2019-12-05 12:07:54 +0100

    Filling Array implemented

--- Fibonacci_Spiel/src/Calculation/Calculator.java
+++ Fibonacci_Spiel/src/Calculation/Calculator.java
@@ -8,4 +8,17 @@ package Calculation;
 
 public class Calculator {
 
+	int [] fibunacciNumbers;
+	
+	public void fillArray() {
+		for (int i = 0; i < 101; i++) {
+			if (i < 2) {
+				fibunacciNumbers[i] = 1;
+			}else {
+				
+			}
+		}
+	}
+	
+	
 }
